9a180d944d5889678d69346cf80e324320a58f25
Felipe Zumarraga <luis.zumarraga@clearminds-it.com>
2021-04-19 12:56:48 -0500

    Se agrega la componente cuerpo2 con ngClass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { CuerpoComponent } from './cuerpo/cuerpo.component';
+import { Cuerpo2Component } from './cuerpo2/cuerpo2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraComponent,
-    CuerpoComponent
+    CuerpoComponent,
+    Cuerpo2Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cuerpo2/cuerpo2.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cuerpo2',
+  templateUrl: './cuerpo2.component.html',
+  styleUrls: ['./cuerpo2.component.css']
+})
+export class Cuerpo2Component implements OnInit {
+
+  textoActivo:string="";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
